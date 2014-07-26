8e9134b8af34f4398b020d0a5676eab2b87ca538
Aastha <aastha@aastha-DV6-6119TX.(none)>
2014-07-27 00:40:26 +0530

    Active admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  #ActiveAdmin.routes(self), controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
+  ActiveAdmin.routes(self)#, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
