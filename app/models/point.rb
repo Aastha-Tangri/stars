75f25b6ec541de7e33dc40aa2aa2f9f4b9d8513b
Aastha <aastha.tangri@trantorinc.com>
2014-07-31 19:02:03 +0530

    Assoc modification

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,5 +1,5 @@
 class Point < ActiveRecord::Base
   belongs_to :employee, :foreign_key => 'given_by'
-  has_one :category
+  belongs_to :category
   validates :given_to, :project, :task, :manager, :category_id,  :presence => true
 end
